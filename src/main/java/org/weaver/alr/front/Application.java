d02044267b83ae9ce3fe9352a98fdb39ade65748
sungtaek <leesungtaek@gmail.com>
2016-06-27 20:09:29 +0900

    rename

--- src/main/java/org/weaver/alr/front/Application.java
+++ src/main/java/org/weaver/alr/front/Application.java
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "org.weaver.alr.front" })
-public class ALRFrontApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ALRFrontApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
