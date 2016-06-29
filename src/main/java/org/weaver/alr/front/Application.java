456368dea869f0c9072db76a0884a02ec3eea982
sungtaek <leesungtaek@gmail.com>
2016-06-29 17:02:20 +0900

    split config

--- src/main/java/org/weaver/alr/front/Application.java
+++ src/main/java/org/weaver/alr/front/Application.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "org.weaver.alr.front" })
 @EnableAutoConfiguration
-@PropertySource("classpath:system-local.properties")
 public class Application {
 
 	public static void main(String[] args) {
