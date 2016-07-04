4b1b4783bd253b604d9b64cfa4d519d24cc727e5
sungtaek <leesungtaek@gmail.com>
2016-07-04 17:10:21 +0900

    remove count

--- src/main/java/org/weaver/alr/front/news/model/MetadataListResponse.java
+++ src/main/java/org/weaver/alr/front/news/model/MetadataListResponse.java
@@ -4,15 +4,8 @@ import org.weaver.alr.front.common.model.Page;
 import org.weaver.alr.front.common.model.Response;
 
 public class MetadataListResponse extends Response {
-	private Integer metadataCount;
 	private Page<Metadata> metadataList;
 
-	public Integer getMetadataCount() {
-		return metadataCount;
-	}
-	public void setMetadataCount(Integer metadataCount) {
-		this.metadataCount = metadataCount;
-	}
 	public Page<Metadata> getMetadataList() {
 		return metadataList;
 	}
